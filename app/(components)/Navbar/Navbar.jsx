85193359b0ca97ca2d8b2c2ce857feca37346f86
Mohamed Motea <mrte3a@gmail.com>
2024-01-06 20:11:55 +0200

    navbar

--- app/(components)/Navbar/Navbar.jsx
+++ app/(components)/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React, {  useEffect } from 'react'
+import React, {  useEffect, useState } from 'react'
 
 const links = [
   {title:'الصفحة الرئيسية',go:'/'},
@@ -20,7 +20,7 @@ export default function Navbar() {
   let path = usePathname().split('/')
 
   let linkStyle = 'text-blue-300'
-  
+  const [hide,setHide] = useState(false)
   useEffect(()=>{
     document.addEventListener('scroll',()=>{
       if(window.scrollY > 30){
@@ -34,19 +34,32 @@ export default function Navbar() {
   const scale = 'hover:scale-[1.02] cursor-pointer'
   
 
-  return <header className='sticky top-0 py-4 px-3 z-10'>
-      <nav>
+  return <header className='sticky top-0 py-4 px-3 z-10 '>
+      <nav className='relative'>
 
-    <div className='flex justify-between items-center w-full'>
+    <div className='flex justify-between items-center w-full '>
 
 
-  <ul className='flex  justify-between font-bold text-center mx-auto' dir='rtl'>
+    <button className='btn sm:hidden ' onClick={()=> hide ? setHide(false) : setHide(true)}>
+    <i class="fa-solid fa-bars text-2xl"></i>
+
+      <div className='absolute bg-[--subColor] rounded-3xl w-[70vh] top-full mt-5 left-[50%] translate-x-[-50%]'>
+
+    {hide ?  <ul className='flex font-bold text-center py-5  mt-5  flex-col text-2xl ' dir='rtl'>
+      {links.map((link,index)=>{ return <Link key={index} href={link.go} className={`${path[1] === link.go.split('/')[1] ? linkStyle : null } text-base my-3`}>{link.title}</Link>} )}
+      </ul> : ''
+    }
+  
+      </div>
+
+    </button>
+
+
+  <ul className='hidden sm:flex justify-between font-bold text-center mx-auto' dir='rtl'>
     {links.map((link,index)=>{ return <Link key={index} href={link.go} className={`${path[1] === link.go.split('/')[1] ? linkStyle : null } mx-2 md:mx-4 text-[15px] md:text-[17px] lg:text-[18px] `}>{link.title}</Link>} )}
-
   </ul>
 
 <div>
-
       <i className={`${scale} fa-regular fa-bell bg-[#0f0f0f] p-2 rounded-full`}></i>
       <i className={`${scale} fa-regular fa-user ml-2 bg-[#0f0f0f] p-2 rounded-full`}></i>
 </div>
